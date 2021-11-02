352389c66c469148bf0fc55002824bc6923de2bc
Priya Yasasvin <priyayasasvin@priyas-air.home>
2021-11-02 19:34:12 +0000

    user signin

--- models/user.js
+++ models/user.js
@@ -3,10 +3,10 @@
 // MONGODB CONNECTION: mongodb+srv://Piriya:<password>@cluster0.v0k8d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const mongoose = require('mongoose');
 const validator = require('mongoose-unique-validator');
-const User = mongoose.Schema({
+const userSchema = mongoose.Schema({
+     userId: { type: String,  },
     email: { type: String, required: true, unique: true },
-    userId: { type: String, required: true },
     password: { type: String, required: true },
 });
-User.plugin(validator);
-module.exports = mongoose.model('User', User);
+userSchema.plugin(validator);
+module.exports = mongoose.model('User', userSchema);
