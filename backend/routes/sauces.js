836ab13dbfa8ee14d89ec77be232034fb32b1895
Priya Yasasvin <priyayasasvin@priyas-air.home>
2021-11-16 20:31:43 +0000

    likes

---
+++ backend/routes/sauces.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const saucesCtrl = require('../controller/sauces');
+
+router.get('/', auth, saucesCtrl.getAllSauces);
+router.post('/', multer, saucesCtrl.createsauces);
+router.get('/:id', auth, saucesCtrl.getOnesauces);
+router.put('/:id', auth,multer, saucesCtrl.modifysauces);
+router.delete('/:id', auth, saucesCtrl.deletesauces);
+router.post('/:id/like', auth, saucesCtrl.likeSauces);
+
+module.exports = router;
